39f33d8c7003b857b0cfe442cc977124a2d6b110
AbdulrahmanIsmailMohamed <abdulrahman.ismail.mohammed@gmail.com>
2024-03-12 00:36:59 +0200

    update routed

--- js/app.js
+++ js/app.js
@@ -1,90 +1,4 @@
-/*const geoserverUrl = "http://localhost:8080/geoserver";
-let selectedPoint = null,
-  source = null,
-  target = null;
-
-// empty geojson layer for the shortes path result
-let pathLayer = L.geoJSON(null);
-
-// initialize our map
-var map = L.map("map", {
-  center: [32.7784, -96.78946], //center map to jkuat
-  zoom: 17, //set the zoom level
-});
-
-//add openstreet baselayer to the map
-var OpenStreetMap = L.tileLayer(
-  "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }
-).addTo(map);
-
-const sourceMarker = L.marker([32.7778316, -96.7912601], { draggable: true })
-  .on("dragend", (e) => {
-    selectedPoint = e.target.getLatLng();
-    getVertex(selectedPoint);
-    getRoute();
-  })
-  .addTo(map);
-
-const targetMarker = L.marker([32.779, -96.79], { draggable: true })
-  .on("dragend", (e) => {
-    selectedPoint = e.target.getLatLng();
-    getVertex(selectedPoint);
-    getRoute();
-  })
-  .addTo(map);
-
-const getVertex = (selectedPoint) => {
-  const { lat, lng } = selectedPoint;
-  const wmsUrl = `${geoserverUrl}/routed/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=routed:nearest_vertex&outputformat=application/json&viewparams=x:${lng};y:${lat};`;
-
-  $.ajax({
-    url: wmsUrl,
-    async: false,
-    success: (data) => {
-      console.log(data);
-      loadVertex(
-        data,
-        selectedPoint.toString() === sourceMarker.getLatLng().toString()
-      );
-    },
-  });
-};
-
-// function to update the source and target nodes as returned from geoserver for later querying
-const loadVertex = (response, isSource) => {
-  if (response.features) {
-    let features = response.features;
-    console.log("Features:", features);
-    map.removeLayer(pathLayer);
-    if (isSource) {
-      source = features[0].properties.id;
-    } else {
-      target = features[0].properties.id;
-    }
-  } else {
-    console.error("Response does not contain features array.");
-  }
-};
-
-// function to get the shortest path from the give source and target nodes
-function getRoute() {
-  let url = `${geoserverUrl}/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=routed:shortest_path&outputformat=application/json&viewparams=source:${source};target:${target};`;
-
-  $.getJSON(url, function (data) {
-    map.removeLayer(pathLayer);
-    pathLayer = L.geoJSON(data);
-    map.addLayer(pathLayer);
-  });
-}
-
-getVertex(sourceMarker.getLatLng());
-getVertex(targetMarker.getLatLng());
-getRoute();*/
+// import zag from "../data/zag.json";
 
 const geoserverUrl = "http://localhost:8080/geoserver";
 let selectedPoint = null,
@@ -97,7 +11,7 @@ let pathLayer = L.geoJSON(null);
 // initialize our map
 var map = L.map("map", {
   center: [30.5904, 31.50673], // center map to jkuat
-  zoom: 17, // set the zoom level
+  zoom: 13, // set the zoom level
 });
 
 // add openstreet baselayer to the map
@@ -109,6 +23,84 @@ var OpenStreetMap = L.tileLayer(
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }
 ).addTo(map);
+
+// display zag points
+
+fetch("../data/zag.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Now you can use the 'data' object containing your JSON
+    initItems(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON:", error);
+  });
+
+const initItems = (zag) => {
+  const customIconHospital = L.icon({
+    iconUrl: "./public/hospital-location-pin.png",
+
+    iconSize: [38, 40], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
+
+  const customIconPolice = L.icon({
+    iconUrl: "./public/police.png",
+
+    iconSize: [38, 35], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
+
+  const customIconAmbulance = L.icon({
+    iconUrl: "./public/ambulance.png",
+
+    iconSize: [38, 35], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
+
+  const customIconFire = L.icon({
+    iconUrl: "./public/fire-station.png",
+
+    iconSize: [40, 35], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
+
+  let icon;
+
+  zag.features.map((item) => {
+    if (item.properties.type === "hospital") {
+      icon = customIconHospital;
+    } else if (item.properties.type === "police") {
+      icon = customIconPolice;
+    } else if (item.properties.type === "Ambulance") {
+      icon = customIconAmbulance;
+    } else {
+      icon = customIconFire;
+    }
+
+    return L.marker(
+      [item.geometry.coordinates[1], item.geometry.coordinates[0]],
+      {
+        icon,
+      }
+    )
+      .bindPopup(
+        createPopupContent(
+          [
+            `Type: ${item.properties.type}`,
+            `Name: ${item.properties.name}`,
+            `Address: ${item.properties.address}`,
+          ],
+          item.properties.photo.replace("\\", "/")
+        )
+      )
+      .addTo(map);
+  });
+};
 
 const sourceMarker = L.marker([30.5904, 31.50673], { draggable: true })
   .on("dragend", (e) => {
@@ -127,13 +119,24 @@ const targetMarker = L.marker([30.5904, 31.50673], { draggable: true })
   .addTo(map);
 
 // Function to create popup content
-function createPopupContent(properties) {
+function createPopupContent(properties, pathPhoto) {
   let popupContent = "<table>";
+
   for (let key in properties) {
     if (properties.hasOwnProperty(key)) {
       popupContent += `<tr><td>${properties[key]}</td></tr>`;
     }
   }
+
+  if (pathPhoto) {
+    popupContent += `
+      <tr>
+        <td style="text-align: center;">
+          <img src="./public${pathPhoto}" alt="photo" weidth="250px" style="max-width: 250px; max-height: 100px;">
+        </td>
+      </tr>`;
+  }
+
   popupContent += "</table>";
   return popupContent;
 }
@@ -161,7 +164,6 @@ const getVertex = (selectedPoint) => {
     url: wmsUrl,
     async: false,
     success: (data) => {
-      console.log(data);
       loadVertex(
         data,
         selectedPoint.toString() === sourceMarker.getLatLng().toString()
@@ -174,9 +176,6 @@ const getVertex = (selectedPoint) => {
 const loadVertex = (response, isSource) => {
   if (response.features) {
     let features = response.features;
-    features.map((item) => {
-      console.log(item.geometry.coordinates);
-    });
     map.removeLayer(pathLayer);
     if (isSource) {
       source = features[0].properties.id;
